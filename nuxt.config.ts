dc8b70a31ed7f4768ffda0348734ca4ff2a7e6f6
valetzx <45054400+valetzx@users.noreply.github.com>
2022-01-09 11:59:30 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
     privateRuntimeConfig: {
         CLIENT_ID: process.env.CLIENT_ID,
         CLIENT_SECRET: process.env.CLIENT_SECRET,
-        REDIRECT_URI: 'http://localhost:3000',
+        REDIRECT_URI: 'http://localhost:3008',
         REFRESH_TOKEN: process.env.REFRESH_TOKEN,
         ROOT_PATH: '/Blog',
         ONEDRIVE_URI: 'https://graph.microsoft.com/v1.0/me/drive',
